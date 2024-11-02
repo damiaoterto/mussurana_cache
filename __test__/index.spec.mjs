6b44ff8b33913183d174591e45de3494ac291e0f
Damião Junior Terto <contato@damiaoterto.dev>
2024-11-02 18:58:24 -0300

    chore: initial tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,32 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { MussuranaCache } from '../index.js'
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test('create cache manager instance', (t) => {
+    const cache = new MussuranaCache()
+    t.true(cache !== undefined)
+    t.assert(cache instanceof MussuranaCache)
+})
+
+test('create cache entry', (t) => {
+    const cache = new MussuranaCache()
+    const entry = { key: 'test-item', value: 'lorem ipsum' }
+
+    cache.set(entry.key, entry.value)
+
+    const existsEntry = cache.get(entry.key)
+
+    t.assert(existsEntry !== null)
+    t.assert(existsEntry === entry.value)
+})
+
+test('return undefined if not found entry', (t) => {
+    const cache = new MussuranaCache()
+    const entry = { key: 'test-item', value: 'lorem ipsum' }
+
+    cache.set(entry.key, entry.value)
+
+    const existsEntry = cache.get('test')
+
+    t.assert(existsEntry === null)
 })
